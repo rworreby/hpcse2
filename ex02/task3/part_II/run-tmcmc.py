14abe948d3734e32368a9cee8f791c5ffe6301e7
Robin Worreby <rworreby@student.ethz.ch>
2021-03-28 16:36:12 +0200

    Fix wrong model name in RBC TMCMC

--- ex02/task3/part_II/run-tmcmc.py
+++ ex02/task3/part_II/run-tmcmc.py
@@ -23,7 +23,7 @@ def main():
     e["Problem"]["Type"] = "Bayesian/Reference"
     e["Problem"]["Likelihood Model"] = "Normal"
     e["Problem"]["Reference Data"] = getReferenceData()
-    e["Problem"]["Computational Model"] = lambda sampleData: model(sampleData, getReferencePoints())
+    e["Problem"]["Computational Model"] = lambda sampleData: model_3p(sampleData, getReferencePoints())
 
     # Configuring TMCMC parameters
     e["Solver"]["Type"] = "Sampler/TMCMC"
