f4a6d2d11d365fc10843f230adc7a729c2bcc788
Robin Worreby <rworreby@student.ethz.ch>
2021-03-28 16:32:34 +0200

    Implement ex 02 task 2 part 2

--- ex02/task3/part_II/run-tmcmc.py
+++ ex02/task3/part_II/run-tmcmc.py
@@ -13,13 +13,71 @@ import sys
 sys.path.append('./_model')
 from model import model_3p, getReferenceData, getReferencePoints
 
+import korali
 
 
-# TODO: Implement korali's configuration
-# by adapting examples/example_3/run-tmcmc.py
-# ...
+def main():
+    e = korali.Experiment()
+
+    # Setting up the reference likelihood for the Bayesian Problem
+    e["Problem"]["Type"] = "Bayesian/Reference"
+    e["Problem"]["Likelihood Model"] = "Normal"
+    e["Problem"]["Reference Data"] = getReferenceData()
+    e["Problem"]["Computational Model"] = lambda sampleData: model(sampleData, getReferencePoints())
+
+    # Configuring TMCMC parameters
+    e["Solver"]["Type"] = "Sampler/TMCMC"
+    e["Solver"]["Population Size"] = 5000
+    e["Solver"]["Target Coefficient Of Variation"] = 0.5
+    e["Solver"]["Covariance Scaling"] = 0.04
+
+    # Configuring the problem's random distributions
+    e["Distributions"][0]["Name"] = "Uniform 4 12"
+    e["Distributions"][0]["Type"] = "Univariate/Uniform"
+    e["Distributions"][0]["Minimum"] = +4.0
+    e["Distributions"][0]["Maximum"] = +12.0
+
+    e["Distributions"][1]["Name"] = "Uniform 05 05"
+    e["Distributions"][1]["Type"] = "Univariate/Uniform"
+    e["Distributions"][1]["Minimum"] = -0.5
+    e["Distributions"][1]["Maximum"] = +0.5
+
+    e["Distributions"][2]["Name"] = "Uniform 01 01"
+    e["Distributions"][2]["Type"] = "Univariate/Uniform"
+    e["Distributions"][2]["Minimum"] = -0.1
+    e["Distributions"][2]["Maximum"] = +0.1
+
+    e["Distributions"][3]["Name"] = "Uniform 0 5"
+    e["Distributions"][3]["Type"] = "Univariate/Uniform"
+    e["Distributions"][3]["Minimum"] = +0.0
+    e["Distributions"][3]["Maximum"] = +5.0
+
+    # Configuring the problem's variables
+    e["Variables"][0]["Name"] = "D0"
+    e["Variables"][0]["Prior Distribution"] = "Uniform 4 12"
+    e["Variables"][0]["Initial Value"] = +8.0
+    e["Variables"][0]["Initial Standard Deviation"] = +0.8
+
+    e["Variables"][1]["Name"] = "k1"
+    e["Variables"][1]["Prior Distribution"] = "Uniform 05 05"
+    e["Variables"][1]["Initial Value"] = +0.0
+    e["Variables"][1]["Initial Standard Deviation"] = +0.05
+
+    e["Variables"][2]["Name"] = "k2"
+    e["Variables"][2]["Prior Distribution"] = "Uniform 01 01"
+    e["Variables"][2]["Initial Value"] = +0.0
+    e["Variables"][2]["Initial Standard Deviation"] = +0.01
+
+    e["Variables"][3]["Name"] = "sig"
+    e["Variables"][3]["Prior Distribution"] = "Uniform 0 5"
+    e["Variables"][3]["Initial Value"] = +2.5
+    e["Variables"][3]["Initial Standard Deviation"] = +0.5
+
+    e["Store Sample Information"] = True
+
+    k = korali.Engine()
+    k.run(e)
 
 
-
-k = korali.Engine()
-k.run(e)
+if __name__ == '__main__':
+    main()
