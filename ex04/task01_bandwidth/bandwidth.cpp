efc7336aab770b9aa31e0e02a52fe4c48039c446
Robin Worreby <rworreby@student.ethz.ch>
2021-05-01 15:29:14 +0200

    Implement outer loop badnwidth measurements

--- ex04/task01_bandwidth/bandwidth.cpp
+++ ex04/task01_bandwidth/bandwidth.cpp
@@ -1,9 +1,41 @@
-// File       : benchmark.cpp
-// Created    : Tue Apr 13 2021 05:31:24 PM (+0200)
-// Description: Benchmark for MPI point-to-point bandwidth
-// Copyright 2021 ETH Zurich. All Rights Reserved.
+#include <iostream>
+#include "mpi.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
+    double *buf = (double *) malloc( n * sizeof(double) );
+
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+
+    for (size_t i = 1; i <= 16777216; i*=2) {
+        std::cout << "Running measurements for " << i << '\n';
+        starttime = MPI_Wtime();
+
+
+        if (rank == 0) {
+            MPI_Send(
+                buf,
+                i,
+                MPI_BOOL,
+                1,
+                1,
+                MPI_COMM_WORLD
+            )
+
+        }
+
+
+        endtime = MPI_Wtime();
+        std::cout << i << "," << (endtime - starttime) << std::endl;
+
+    }
+
+
+
     return 0;
 }
