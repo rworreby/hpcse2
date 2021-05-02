3286852ffe0480fb4d904a7075841fe53acd10b7
Robin Worreby <rworreby@student.ethz.ch>
2021-05-02 16:05:26 +0200

    Fix undeclared variables

--- ex04/task01_bandwidth/bandwidth.cpp
+++ ex04/task01_bandwidth/bandwidth.cpp
@@ -3,17 +3,16 @@
 
 int main(int argc, char **argv)
 {
-    double *buf = (double *) malloc( n * sizeof(double) );
-
     MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    for (size_t i = 1; i <= 16777216; i*=2) {
+    for (size_t i = 1; i <= 16777216; i*=2) {.
+        double *buf = (double *) malloc( i * sizeof(double) );
         std::cout << "Running measurements for " << i << '\n';
-        starttime = MPI_Wtime();
+        double starttime = MPI_Wtime();
 
         int loop_repetitions = 1024 / n;
         if (loop_repetitions < 1){
@@ -45,8 +44,8 @@ int main(int argc, char **argv)
             }
         }
 
-        endtime = MPI_Wtime();
-        auto time = (endtime - starttime) / loop_repetitions;
+        double endtime = MPI_Wtime();
+        double time = (endtime - starttime) / loop_repetitions;
         std::cout << i << "," << time << std::endl;
     }
 
