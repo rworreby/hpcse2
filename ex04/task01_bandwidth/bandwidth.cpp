81ae10384afb512366e2cfd7ec2ec170ccfaf3ca
Robin Worreby <rworreby@student.ethz.ch>
2021-05-02 16:25:06 +0200

    Add MPI_Finalize at end of mpi section

--- ex04/task01_bandwidth/bandwidth.cpp
+++ ex04/task01_bandwidth/bandwidth.cpp
@@ -49,6 +49,7 @@ int main(int argc, char **argv)
         double time = (endtime - starttime) / loop_repetitions;
         std::cout << i << "," << time << std::endl;
     }
-
+    
+    MPI_Finalize();
     return 0;
 }
