915cbd12f87063eeb53b89077b409d51089fb6aa
Robin Worreby <rworreby@student.ethz.ch>
2021-05-02 16:01:04 +0200

    Add nested loop for better benchmarking

--- ex04/task01_bandwidth/bandwidth.cpp
+++ ex04/task01_bandwidth/bandwidth.cpp
@@ -11,31 +11,44 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-
     for (size_t i = 1; i <= 16777216; i*=2) {
         std::cout << "Running measurements for " << i << '\n';
         starttime = MPI_Wtime();
 
-
-        if (rank == 0) {
-            MPI_Send(
-                buf,
-                i,
-                MPI_BOOL,
-                1,
-                1,
-                MPI_COMM_WORLD
-            )
-
+        int loop_repetitions = 1024 / n;
+        if (loop_repetitions < 1){
+            loop_repetitions = 1;
         }
 
+        for (size_t k = 0; k < loop_repetitions; k++) {
+
+            if (rank == 0) {
+                MPI_Send(
+                    buf,
+                    i,
+                    MPI_BOOL,
+                    1,
+                    1,
+                    MPI_COMM_WORLD
+                )
+            }
+
+            if (rank == 1) {
+                MPI_Recv(
+                    buf, // buffer
+                    i, // count
+                    MPI_BOOL, // datatype
+                    0, // source
+                    1, //tag
+                    MPI_COMM_WORLD // communicator
+                )
+            }
+        }
 
         endtime = MPI_Wtime();
-        std::cout << i << "," << (endtime - starttime) << std::endl;
-
+        auto time = (endtime - starttime) / loop_repetitions;
+        std::cout << i << "," << time << std::endl;
     }
-
-
 
     return 0;
 }
